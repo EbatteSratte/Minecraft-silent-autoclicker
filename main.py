4660e2bb1b48c14670d47a02b39f69fdf2f64010
Ebatte_Sratte <93819705+EbatteSratte@users.noreply.github.com>
2024-03-25 00:14:49 +0300

    update

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ config = ConfigParser()
 config.read("config.ini")
 
 
-def click(x, y):
-    hWnd = win32gui.FindWindow(None, "Minecraft* 1.20.2 - Multiplayer (3rd-party Server)")
+def click(x, y, type):
+    hWnd = win32gui.FindWindow(None, config.get("main", "windowName"))
     lParam = win32api.MAKELONG(x, y)
-    win32api.SendMessage(hWnd, win32con.WM_RBUTTONDOWN, win32con.MK_RBUTTON, lParam)
-    win32api.SendMessage(hWnd, win32con.WM_RBUTTONUP, win32con.MK_RBUTTON, lParam)
-    time.sleep(1)
+    if (type == "right"):
+        win32api.SendMessage(hWnd, win32con.WM_RBUTTONDOWN, win32con.MK_RBUTTON, lParam)
+        win32api.SendMessage(hWnd, win32con.WM_RBUTTONUP, win32con.MK_RBUTTON, lParam)
+    elif (type == "left"):
+        win32api.SendMessage(hWnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+        win32api.SendMessage(hWnd, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, lParam)
     print(f"Clicked! Time: " + str(datetime.now()).split(" ")[1])
 
 
 while True:
-    click(int(config.get("main", "xCoord")), int(config.get("main", "yCoord")))
+    click(int(config.get("main", "xCoord")), int(config.get("main", "yCoord")), config.get("main", "type"))
     time.sleep(int(config.get("main", "delay")))
